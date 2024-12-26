25ef62bfe4f8a52fe3da26b62cbe0a09fc042cdc
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-12-26 07:50:25 -0300

    Avances

---
+++ src/app/components/Campos/mostrar-marcas/mostrar-marcas.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'MostrarMarcas',
+  standalone: true,
+  imports: [],
+  templateUrl: './mostrar-marcas.component.html',
+})
+export class MostrarMarcasComponent {
+
+
+  showAddMarcas = false
+}
--- src/app/pages/editar-campos/editar-campos.component.ts
+++ src/app/pages/editar-campos/editar-campos.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { CardActionsComponent } from '../../components/card-actions/card-actions.component';
+import { MostrarMarcasComponent } from '../../components/Campos/mostrar-marcas/mostrar-marcas.component';
+import { CommonModule } from '@angular/common';  // Importa CommonModule
+
 
 @Component({
   selector: 'app-editar-campos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SidebarComponent, CardActionsComponent, MostrarMarcasComponent],
   templateUrl: './editar-campos.component.html',
-  styleUrl: './editar-campos.component.css'
+  // styleUrl: './editar-campos.component.css'
 })
 export class EditarCamposComponent {
+
+  opcionesDatos = [
+    { texto: "Marcas" },
+    { texto: "Tipos" },
+    { texto: "Formularios"},
+    { texto: "Valores"},
+    { texto: "Bancos"},
+    { texto: "Numerador"}
+  ]
+  opcion:string = ""
+  
+  
+  getClickedSection(text: any) {
+    console.log(text)
+
+      this.opcion = text
+  }
 
 }
