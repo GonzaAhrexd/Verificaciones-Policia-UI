c162db013f734e3e61d3fb52fe6a4c6faf173221
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-12-27 12:59:36 -0300

    DataTable

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 // Componentes
 import { CardActionsComponent } from '../../components/card-actions/card-actions.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
-import { TableExpandableRowsExample } from '../../components/table-expandable-rows-example/table-expandable-rows-example.component';
+import { MostrarMarcasComponent } from '../../components/table-expandable-rows-example/table-expandable-rows-example.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardActionsComponent, SidebarComponent, TableExpandableRowsExample],
+  imports: [CardActionsComponent, SidebarComponent, MostrarMarcasComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
