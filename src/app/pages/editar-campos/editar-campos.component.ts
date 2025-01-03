51070f7c0c719c74a5456272dba24fcbff5d2cce
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2025-01-03 08:38:42 -0300

    Avances

--- src/app/pages/editar-campos/editar-campos.component.ts
+++ src/app/pages/editar-campos/editar-campos.component.ts
@@ -5,19 +5,17 @@ import { CardActionsComponent } from '../../components/card-actions/card-actions
 
 
 // Campos
-import { MostrarMarcasComponent } from '../../components/Campos/mostrar-marcas/mostrar-marcas.component';
 import { MostrarTiposComponent } from '../../components/Campos/mostrar-tipos/mostrar-tipos.component';
 import { MostrarFormulariosComponent } from '../../components/Campos/mostrar-formularios/mostrar-formularios.component';
 import { MostrarValoresComponent } from '../../components/Campos/mostrar-valores/mostrar-valores.component';
 import { MostrarBancosComponent } from '../../components/Campos/mostrar-bancos/mostrar-bancos.component';
-import { MostrarNumeradorComponent } from '../../components/Campos/mostrar-numerador/mostrar-numerador.component';
 
 @Component({
   selector: 'app-editar-campos',
   standalone: true,
   imports: [CommonModule, SidebarComponent, CardActionsComponent,
     // Campos
-     MostrarMarcasComponent, MostrarTiposComponent, MostrarFormulariosComponent, MostrarValoresComponent, MostrarBancosComponent, MostrarNumeradorComponent],
+      MostrarTiposComponent, MostrarFormulariosComponent, MostrarValoresComponent, MostrarBancosComponent],
   templateUrl: './editar-campos.component.html',
 })
 export class EditarCamposComponent {
@@ -27,14 +25,16 @@ export class EditarCamposComponent {
     { texto: "Formularios"},
     { texto: "Valores"},
     { texto: "Bancos"},
+    
   ]
+
+  
   opcion:string = ""
   
-  
+
   getClickedSection(text: any) {
     console.log(text)
 
       this.opcion = text
   }
-
 }
